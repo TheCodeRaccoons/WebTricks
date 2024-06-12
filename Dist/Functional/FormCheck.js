3565a4f57022a5518ef26cff07a42edc8fadfa21
Joorch <jacortez_94@hotmail.com>
2024-06-12 14:43:32 -0600

    [Script updates] Added elipsis.css and updated formcheck.js
    Elipsis.css will give a super small visual update allowing the user to set a text to a max of 1,2 or 3 lines.
    
    Updated the formcheck script to use classes so the script is better optimized and we can encapsulate the functionalities or trigger them if needed. also added a clear form function that can be triggered through the class or throguh a button if added according to the documentation.

--- Dist/Functional/FormCheck.js
+++ Dist/Functional/FormCheck.js
@@ -1,68 +1,112 @@
 'use strict'
 
-const InitializeFormCheck = () => {
-    let pattern = /^[+]*[(]{0,1}[0-9]{1,3}[)]{0,1}[-\s\./0-9]*$/g;
-    var checkForms = document.querySelectorAll('[wt-formcheck-element="form"]');
-    if (!checkForms || checkForms.length === 0) return;
-    for (var form of checkForms) {
-        var errorClass = form.getAttribute("wt-formcheck-class");
-        var submitMessage = form.getAttribute("wt-formcheck-message");
-        var requiredFields = form.querySelectorAll('[wt-formcheck-type="required"]');
-        var errorElements = form.querySelectorAll('[wt-formcheck-type="error"]');
-        var submitButton = form.querySelector('[wt-formcheck-type="submit"]');
-        var requiredFieldsAndCheckboxes = form.querySelectorAll('[wt-formcheck-type="required"], [type="checkbox"]');
-        var formErrors = false;
+class FormCheck {
+    constructor(form) {
+        this.form = form;
+        this.pattern = /^[+]*[(]{0,1}[0-9]{1,3}[)]{0,1}[-\s\./0-9]*$/g;
+        this.errorClass = form.getAttribute("wt-formcheck-class");
+        this.submitMessage = form.getAttribute("wt-formcheck-message");
+        this.requiredFields = form.querySelectorAll('[wt-formcheck-type="required"]');
+        this.errorElements = form.querySelectorAll('[wt-formcheck-type="error"]');
+        this.submitButton = form.querySelector('[wt-formcheck-type="submit"]');
+        this.defaultSubmitText = this.submitButton.textContent;
+        this.resetButton = form.querySelector('[wt-formcheck-element="reset"]');
+        this.requiredFieldsAndCheckboxes = form.querySelectorAll('[wt-formcheck-type="required"], [type="checkbox"]');
+        this.formErrors = false;
 
-        for (let element of errorElements) {
+        this.init();
+    }
+
+    init() {
+        for (let element of this.errorElements) {
             element.style.display = 'none';
         }
 
-        for (var field of requiredFields) {
-            field.classList.remove(`${errorClass}`);
-        };
-
-        let fieldError = (field) => {
-            field.parentNode.querySelector('[wt-formcheck-type="error"]').style.display = 'block';
-            field.classList.add(`${errorClass}`);
-            formErrors = true;
+        for (let field of this.requiredFields) {
+            field.classList.remove(`${this.errorClass}`);
         }
 
-        for (var field of requiredFieldsAndCheckboxes) { 
-            field.addEventListener('keypress', (e) => {
-                if (errorClass) e.target.classList.remove(`${errorClass}`);
-                e.target.parentNode.querySelector('[wt-formcheck-type="error"]').style.display = 'none';
-                formErrors = false;
-            });
+        for (let field of this.requiredFieldsAndCheckboxes) {
+            field.addEventListener('keypress', (e) => this.clearError(e));
+            field.addEventListener('blur', (e) => this.clearError(e));
+        }
 
-            field.addEventListener('blur', (e) => {
-                if (errorClass) e.target.classList.remove(`${errorClass}`);
-                e.target.parentNode.querySelector('[wt-formcheck-type="error"]').style.display = 'none';
-                formErrors = false;
-            });
-        };
+        this.submitButton.addEventListener('click', (e) => this.validateAndSubmit(e));
 
-        submitButton.addEventListener('click', () => {
-            formErrors = false;
-            for (let field of requiredFields) {
-                if (field.getAttribute('type') === 'checkbox' && !field.checked) {
-                    fieldError(field.nextElementSibling);
-                } else if (field.value.trim() === '') {
-                    fieldError(field);
-                } else if (field.getAttribute('type') === 'email' && (field.value.indexOf('@') === -1 || field.value.indexOf('.') === -1)) {
-                    fieldError(field);
-                } else if (field.getAttribute('type') === 'number' && field.value.trim() === '' && !isNaN(field.value.trim())) {
-                    fieldError(field);
-                } else if (field.getAttribute('type') === 'tel' && !field.value.match(pattern)) {
-                    fieldError(field);
-                }
-            };
+        if (this.resetButton) {
+            this.resetButton.addEventListener('click', (e) => this.clearForm(e));
+        }
+    }
 
-            if (!formErrors) {
-                submitButton.textContent = `${submitMessage}`;
-                form.submit();
+    clearError(e) {
+        const target = e.target;
+        if (this.errorClass) target.classList.remove(`${this.errorClass}`);
+        const errorElement = target.parentNode.querySelector('[wt-formcheck-type="error"]');
+        if (errorElement) {
+            errorElement.style.display = 'none';
+        }
+        this.formErrors = false;
+    }
+
+    fieldError(field) {
+        const errorElement = field.parentNode.querySelector('[wt-formcheck-type="error"]');
+        if (errorElement) {
+            errorElement.style.display = 'block';
+        }
+        field.classList.add(`${this.errorClass}`);
+        this.formErrors = true;
+    }
+
+    validateAndSubmit(e) {
+        this.formErrors = false;
+
+        for (let field of this.requiredFields) {
+            const fieldType = field.getAttribute('type');
+            const fieldValue = field.value.trim();
+
+            if (fieldType === 'checkbox' && !field.checked) {
+                this.fieldError(field);
+            } else if (fieldValue === '') {
+                this.fieldError(field);
+            } else if (fieldType === 'email' && (!this.isValidEmail(fieldValue))) {
+                this.fieldError(field);
+            } else if (fieldType === 'number' && (fieldValue === '' || isNaN(fieldValue))) {
+                this.fieldError(field);
+            } else if (fieldType === 'tel' && !field.value.match(this.pattern)) {
+                this.fieldError(field);
             }
-        });
+        }
+
+        if (this.formErrors) {
+            e.preventDefault();
+        } else {
+            this.submitButton.textContent = `${this.submitMessage}`;
+            this.form.submit();
+        }
+    }
+
+    isValidEmail(email) {
+        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailPattern.test(email);
+    }
+
+    clearForm(e) {
+        e.preventDefault();
+        for (let element of this.errorElements) {
+            element.style.display = 'none';
+        }
+
+        for (let field of this.requiredFields) {
+            field.classList.remove(`${this.errorClass}`);
+        }
+
+        this.form.reset();
+
+        this.submitButton.textContent = `${this.defaultSubmitText}`;
     }
 }
 
-document.addEventListener("DOMContentLoaded", InitializeFormCheck);
+document.addEventListener('DOMContentLoaded', () => {
+    const forms = document.querySelectorAll('[wt-formcheck-element="form"]');
+    forms.forEach(form => new FormCheck(form));
+});
