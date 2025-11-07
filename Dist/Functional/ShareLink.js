e5fa3a0ff5ff45de760aecbb5a3a4fba20fc5cea
Likhitha Kathireddy <likhikreddy@gmail.com>
2025-11-07 16:22:40 -0500

    fix(#57): add copied state and popup alert to Social Share module

--- Dist/Functional/ShareLink.js
+++ Dist/Functional/ShareLink.js
@@ -19,21 +19,30 @@ class ShareLink {
         const socialSelectors = {
             facebook: `https://www.facebook.com/sharer/sharer.php?u=${this.encoded}`,
             twitter: `https://twitter.com/share?url=${this.encoded}`,
-            linkedin: `https://www.linkedin.com/shareArticle?mini=true&url='${this.encoded}`,
+            linkedin: `https://www.linkedin.com/shareArticle?mini=true&url=${this.encoded}`,
             whatsapp: `https://wa.me/?text=${this.encoded}`,
-            pinterest: `http://pinterest.com/pin/create/button/?url=${this.encoded}`,
-            reddit: `http://www.reddit.com/submit?url=${this.encoded}`
+            pinterest: `https://pinterest.com/pin/create/button/?url=${this.encoded}`,
+            reddit: `https://www.reddit.com/submit?url=${this.encoded}`
         };
 
         let _link = socialSelectors[this.platform];
         if(!_link && !this.platform) return; // handle errors if the platform has issues
 
-        if(this.platform === 'copy') {
-            this.element.addEventListener('click', () => navigator.clipboard.writeText(`${this.url}`));
-        }
-        else{
+        // just made a small twick to the orginal code Copies the URL to clipboard. Shows Copied! right after clicking. Reverts to the original text after 1.5 seconds and displays an alert popup saying Link copied to clipboard!
+
+        if (this.platform === 'copy') {
+            this.element.addEventListener('click', () => {
+                navigator.clipboard.writeText(`${this.url}`);
+                const originalText = this.element.innerText;
+                this.element.innerText = 'Copied!';
+                alert('Link copied to clipboard!');
+                setTimeout(() => {
+                    this.element.innerText = originalText;
+                }, 1500);
+            });
+        } else {
             this.element.setAttribute("href", _link);
-            this.element.setAttribute("target", '_blank');
+            this.element.setAttribute("target", "_blank");
         }
     }
 }
